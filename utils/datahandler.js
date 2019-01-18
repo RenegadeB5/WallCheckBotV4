4aeabe5979b17a875ae1b02fac9086934b4b52de
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-17 20:15:41 -0500

    Update datahandler.js

--- utils/datahandler.js
+++ utils/datahandler.js
@@ -31,9 +31,7 @@ class dataHandler {
 			exiled.insertOne({user: user, userid: userid, points: 1});
 		}
 		else {
-			let points = member[0].points;
-			console.log(points);
-			exiled.updateOne({userid: userid}, {user: user, userid: userid, points: points += 1});
+			exiled.updateOne({userid: userid}, {user: user, userid: userid, points: member[0].points + 1});
 		}
 	}
 	async fetchLink(query) {
