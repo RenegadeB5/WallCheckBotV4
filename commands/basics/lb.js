3b938874476b1a9aca736b38070803d059ad8672
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-18 09:31:50 -0500

    Create lb.js

---
+++ commands/basics/lb.js
@@ -0,0 +1,28 @@
+var { Command } = require("discord.js-commando");
+const Discord = require('discord.js');
+module.exports = class PingCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: "clear",
+			description: "Clears the walls.",
+			group: "basics",
+			memberName: "clear"
+		});
+	}
+	
+	async run(msg) {
+		let tag = msg.author.tag;
+		global.client.timers.stop();
+		global.client.timers.start();
+		global.client.datahandler.addPoint(tag, msg.author.id);
+		let embed = new Discord.RichEmbed()
+		.setColor(0x00FF00)
+		.setTitle('Walls Cleared!')
+		.addField('Cleared by:', tag, true)
+		.addField('Insentive:', tag + ' now has ' + (await global.client.datahandler.getPoints(msg.author.id) + 1) + ' points!', true)
+		.setTimestamp()
+		msg.channel.send(embed);
+		global.client.user.setStatus('online');
+		global.client.user.setPresence({ game: { name: 'The walls are safe.', type: 0 } });	
+	}
+};
