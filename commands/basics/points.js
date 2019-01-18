8898cfc97c6b6cdd804afb5f33a2414c800fa48b
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-17 20:51:14 -0500

    Update points.js

--- commands/basics/points.js
+++ commands/basics/points.js
@@ -1,5 +1,4 @@
 var { Command } = require("discord.js-commando");
-const Discord = require('discord.js');
 module.exports = class PingCommand extends Command {
 	constructor(client) {
 		super(client, {
