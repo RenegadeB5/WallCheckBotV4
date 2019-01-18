61f25c034efae4bd6649492d752818e3750d023b
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-18 13:38:20 -0500

    Update datahandler.js

--- utils/datahandler.js
+++ utils/datahandler.js
@@ -43,7 +43,7 @@ class dataHandler {
 		let exiled = this.db.collection("factionPoints");
 		let lb = '';
 		let lbdata = await exiled.find().sort({points: -1}).toArray();
-		for (var i = set; set + 3 >= i; i++) {
+		for (var i = set; set + 2 >= i; i++) {
 			lb = lb + ('\n' + lbdata[i].user + ': ' + lbdata[i].points)
 		}
 		console.log(lb);
