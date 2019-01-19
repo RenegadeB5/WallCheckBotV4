c9b871ffed9ec05b9f3d3df11022f248071067ed
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-19 00:11:18 -0500

    Update lbservice.js

--- services/lbservice.js
+++ services/lbservice.js
@@ -11,7 +11,7 @@ module.exports = {
 			}
 			if(reaction.emoji.name === '◀' && reaction.message.author.id === '407593823921766410') {
 				console.log((reaction.message.embeds[0].title)[18]);
-				console.log((reaction.message.embeds[0].title)[25]);
+				console.log((reaction.message.embeds[0].title)[24]);
 				
 			}
 		}
