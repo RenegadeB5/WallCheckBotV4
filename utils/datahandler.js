34fabcebbf2a3f32e3087952a7e0436c0ad61a86
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-18 21:18:36 -0500

    Update datahandler.js

--- utils/datahandler.js
+++ utils/datahandler.js
@@ -47,7 +47,7 @@ class dataHandler {
 		for (var i = 0; i <= lbdata.length - 1; i++) {
 			lb = lb + ('\n' + lbdata[i].user + ': ' + lbdata[i].points + ' points')
 		}
-		return [lb, pages, page];
+		return [lb, page, pages];
 	}
 }
 
