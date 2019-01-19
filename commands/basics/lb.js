ae5fb1d5c974bd979b8115b20895530de9718a06
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-19 09:39:11 -0500

    Update lb.js

--- commands/basics/lb.js
+++ commands/basics/lb.js
@@ -18,6 +18,6 @@ module.exports = class PingCommand extends Command {
 		.addField('Member: Points', lb[0], true)
 		.setFooter('Insentive text goes here.')
 		.setTimestamp()
-		msg.channel.send(embed).then(function (message) {message.react('◀'), setTimeout(function () {message.react('▶')}, 1000)});;
+		msg.channel.send(embed).then(function (message) {message.react('◀'), setTimeout(function () {message.react('▶')}, 1000)});
 	}
 };
