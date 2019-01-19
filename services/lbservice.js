81bc813794f5cc70b98bd91b1a2e27cf64cdacdc
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-19 01:52:52 -0500

    Update lbservice.js

--- services/lbservice.js
+++ services/lbservice.js
@@ -8,6 +8,14 @@ module.exports = {
 			if (reaction.users.map(r => r.id).slice(-1)[0] === '407593823921766410') return;		
 			if(reaction.emoji.name === '▶' && reaction.message.author.id === '407593823921766410') {
 				if ((reaction.message.embeds[0].title).split(' ')[2] === (reaction.message.embeds[0].title).split(' ')[4]) return;
+				let lb = await global.client.datahandler.getLB((reaction.message.embeds[0].title).split(' ')[2] + 1);
+				let embed = new Discord.RichEmbed()
+				.setColor(0x00FF00)
+				.setTitle('Leaderboard: Page ' + ((reaction.message.embeds[0].title).split(' ')[2]) + 1 ' of ' + lb[1])
+				.addField('Top 10:', lb[0], true)
+				.setFooter('Insentive text goes here.')
+				.setTimestamp()
+				reaction.message.edit(embed);
 			}
 			if(reaction.emoji.name === '◀' && reaction.message.author.id === '407593823921766410') {
 				if ((reaction.message.embeds[0].title).split(' ')[2] === (reaction.message.embeds[0].title).split(' ')[4]) return;
