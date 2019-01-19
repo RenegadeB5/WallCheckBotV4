3158616a2b5894fc23042fdae85eb0e6ab13e2c9
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-19 01:22:00 -0500

    Update addpoint.js

--- commands/basics/addpoint.js
+++ commands/basics/addpoint.js
@@ -2,14 +2,14 @@ var { Command } = require("discord.js-commando");
 module.exports = class PingCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: "points",
-			description: "Gets a user's points.",
+			name: "addpoint",
+			description: "Gives a user points.",
 			group: "basics",
-			memberName: "points"
+			memberName: "addpoint"
 		});
 	}
 	
 	async run(msg) {
-		msg.channel.send('You have ' + await global.client.datahandler.getPoints(msg.author.id) + ' points!');
+		global.client.datahandler.addPoint(msg.mentions.users.first().tag, msg.mentions.users.first().id);
 	}
 };
