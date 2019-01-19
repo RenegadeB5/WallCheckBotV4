0e0714fbdd63c709479c9528c2343673fe7ed7c0
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-19 00:27:49 -0500

    Update datahandler.js

--- utils/datahandler.js
+++ utils/datahandler.js
@@ -37,7 +37,7 @@ class dataHandler {
 	async getPoints(userid) {
 		let exiled = this.db.collection("factionPoints");
 		let member = await exiled.find({userid: userid}).toArray();
-		return member[0].points;
+		return await member[0].points;
 	}
 	async getLB(page) {
 		let exiled = this.db.collection("factionPoints");
