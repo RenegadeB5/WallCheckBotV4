4f2d9b9e59bb39f09c186a5d2447c96dfa959c04
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-19 09:47:45 -0500

    Update timers.js

--- utils/timers.js
+++ utils/timers.js
@@ -57,7 +57,7 @@ class Timers {
 	async weewoo(user) {
 		global.client.timers.stop();
 		function weewooo(user) {
-			for (var i = 0; i <= 5; i++) {
+			for (var i = 1; i <= 5; i++) {
 				global.client.channels.get('533359760989487164').send('@everyone');
 			}
 			let embed = new Discord.RichEmbed()
