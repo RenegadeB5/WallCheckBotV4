f3228a1800f196c2e20b6022e18c3f0afe5e7064
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-19 13:01:57 -0500

    Update give.js

--- commands/basics/give.js
+++ commands/basics/give.js
@@ -12,7 +12,7 @@ module.exports = class PingCommand extends Command {
 	
 	async run(msg) {
 		let args = msg.content.split(" ");
-		global.client.datahandler.addPoint(msg.mentions.users.first().tag, msg.mentions.users.first().id), parseInt(args[2]);
+		global.client.datahandler.addPoint(msg.mentions.users.first().tag, msg.mentions.users.first().id, parseInt(args[2]));
 		msg.channel.send('Successfully gave ' + msg.mentions.users.first().tag + ' ' + args[2] + ' point(s)!');
 	}
 };
