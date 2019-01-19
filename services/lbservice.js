a120d2e0c0c28afeac603e89dbae932954f88270
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-19 00:00:27 -0500

    Update lbservice.js

--- services/lbservice.js
+++ services/lbservice.js
@@ -11,14 +11,7 @@ module.exports = {
 			}
 			if(reaction.emoji.name === '◀' && reaction.message.author.id === '407593823921766410') {
 				console.log(reaction.message.embeds.title);
-				let lb = await global.client.datahandler.getLB();
-				let embed = new Discord.RichEmbed()
-				.setColor(0x00FF00)
-				.setTitle('Leaderboard')
-				.addField('Top 10:', lb[0], true)
-				.setFooter('Showing page 1 of ' + lb[1])
-				.setTimestamp()
-				reaction.message.edit(embed);
+				
 			}
 		}
 	}
