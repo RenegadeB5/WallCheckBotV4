1d5676150caa8e7bc813741847ab16e600f5b873
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-19 12:31:40 -0500

    Update and rename addpoint.js to give.js

--- commands/basics/give.js
+++ commands/basics/give.js
@@ -2,15 +2,16 @@ var { Command } = require("discord.js-commando");
 module.exports = class PingCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: "addpoint",
+			name: "give",
 			description: "Gives a user points.",
 			group: "basics",
-			memberName: "addpoint",
+			memberName: "give",
 			userPermissions: ['ADMINISTRATOR']
 		});
 	}
 	
 	async run(msg) {
-		global.client.datahandler.addPoint(msg.mentions.users.first().tag, msg.mentions.users.first().id);
+		let args = msg.content.split(" ").slice(0);
+		global.client.datahandler.addPoint(msg.mentions.users.first().tag, msg.mentions.users.first().id), args[2];
 	}
 };
