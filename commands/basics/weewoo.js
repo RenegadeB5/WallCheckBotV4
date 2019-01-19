24155bfe524bd4c199c6d9b662231298a702ec4b
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-19 09:37:33 -0500

    Create weewoo.js

---
+++ commands/basics/weewoo.js
@@ -0,0 +1,23 @@
+var { Command } = require("discord.js-commando");
+const Discord = require('discord.js');
+module.exports = class PingCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: "lb",
+			description: "Gets the current leaderboard.",
+			group: "basics",
+			memberName: "lb"
+		});
+	}
+	
+	async run(msg) {
+		let lb = await global.client.datahandler.getLB(1);
+		let embed = new Discord.RichEmbed()
+		.setColor(0x00FFFF)
+		.setTitle('Leaderboard: Page 1 of ' + lb[1])
+		.addField('Member: Points', lb[0], true)
+		.setFooter('Insentive text goes here.')
+		.setTimestamp()
+		msg.channel.send(embed).then(function (message) {message.react('◀'), setTimeout(function () {message.react('▶')}, 1000)});;
+	}
+};
