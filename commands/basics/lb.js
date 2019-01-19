0e91eeabed994c4c583047187df20d9d9c43d4a3
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-19 01:45:16 -0500

    Update lb.js

--- commands/basics/lb.js
+++ commands/basics/lb.js
@@ -11,7 +11,7 @@ module.exports = class PingCommand extends Command {
 	}
 	
 	async run(msg) {
-		let lb = await global.client.datahandler.getLB(0);
+		let lb = await global.client.datahandler.getLB(1);
 		let embed = new Discord.RichEmbed()
 		.setColor(0x00FF00)
 		.setTitle('Leaderboard: Page 1 of ' + lb[1])
