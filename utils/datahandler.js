9bd9f9b5fb9f3529c8c07aa75850299a43b7ac6f
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-18 23:50:28 -0500

    Update datahandler.js

--- utils/datahandler.js
+++ utils/datahandler.js
@@ -46,7 +46,7 @@ class dataHandler {
 		let pages = await Math.floor(lbdata.length / 100 % 10 * 10 + 1);
 		let limit = 0;
 		if (page + 1 === pages) {
-			limit = pages.length;
+			limit = lbdata.length - (page * 10);
 			console.log(limit);
 		}
 		else {
