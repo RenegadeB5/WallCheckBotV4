1cd75174043cf7453bb62aef79410f222141ac90
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-20 13:21:26 -0500

    Update help.js

--- commands/basics/help.js
+++ commands/basics/help.js
@@ -3,10 +3,10 @@ const Discord = require('discord.js');
 module.exports = class HelpCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: "Help",
+			name: "help",
 			description: "Displays all commands.",
 			group: "basics",
-			memberName: "snipe"
+			memberName: "help"
 		});
 	}
 	
