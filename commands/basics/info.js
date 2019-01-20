c6fc3c741c0ccd7322eb46d3305cda02fa70c385
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-20 14:32:29 -0500

    Update info.js

--- commands/basics/info.js
+++ commands/basics/info.js
@@ -14,9 +14,9 @@ module.exports = class InfoCommand extends Command {
 		let embed = new Discord.RichEmbed()
 		.setColor(0x0000FF)
 		.setTitle('Info About This Bot')
-		.addField('Developed by:', 'RenegadeBB#4311', true)
-		.addField('Version:', '4', true)
-		.addField('Prefix:', '.', true)
+		.addField('Developed by:', 'RenegadeBB#4311 and Bokli#0001')
+		.addField('Version:', '4')
+		.addField('Prefix:', '.')
 		.setTimestamp()
 		msg.channel.send(embed);
 	}
