4f0abb9b7192638f72e592ed15bde8eceeca7d71
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-20 01:10:11 -0500

    Update stop.js

--- commands/basics/stop.js
+++ commands/basics/stop.js
@@ -1,5 +1,5 @@
 var { Command } = require("discord.js-commando");
-module.exports = class PingCommand extends Command {
+module.exports = class StopCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: "stop",
