87acc024ebded9e782858d4b001c902ef9a33c39
RenegadeB5 <rocketmanB5@gmail.com>
2019-01-20 13:20:35 -0500

    Update help.js

--- commands/basics/help.js
+++ commands/basics/help.js
@@ -3,20 +3,27 @@ const Discord = require('discord.js');
 module.exports = class HelpCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: "snipe",
-			description: "Snipes the most recently deleted message.",
+			name: "Help",
+			description: "Displays all commands.",
 			group: "basics",
 			memberName: "snipe"
 		});
 	}
 	
 	async run(msg) {
-		let snipe = global.snipe;
-		if (snipe === undefined) return;
 		let embed = new Discord.RichEmbed()
-		.setColor(0x0000FF)
+		.setColor(0xBF00FF)
 		.setTitle('Message sniped!')
-		.addField(snipe[0] + ':', snipe[1], true)
+		.addField('.clear', 'Clears the walls. Usage: .clear', true)
+		.addField('.give', 'Gives the mentioned player x ammount of points. Usage: .give <mention> <points>', true)
+		.addField('.help', 'Displays all commands with their description and correct usage. Usage: .help', true)
+		.addField('.info', 'Displays the bot\'s info. Usage: .info', true)
+		.addField('.lb', 'Displays the points leaderboard. Usage: .lb', true)
+		.addField('.points', 'Displays the user\'s points. Usage: .points', true)
+		.addField('.snipe', 'Displays the most recently deleted message. Usage: .snipe', true)
+		.addField('.start', 'Starts the wall service. Usage: .start', true)
+		.addField('.stop', 'Stops the wall service. Usage: .stop', true)
+		.addField('.weewoo', 'Alerts the faction that we are being raided. Usage: .weewoo', true)
 		.setTimestamp()
 		msg.channel.send(embed);
 	}
