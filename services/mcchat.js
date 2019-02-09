b5090c9d8c1325b6b0058698aa8851221836abaa
RenegadeB5 <rocketmanB5@gmail.com>
2019-02-09 00:44:20 -0500

    Update mcchat.js

--- services/mcchat.js
+++ services/mcchat.js
@@ -1,13 +1,15 @@
-global.bot.on("message", function(message) {
-	var msg = message.toString();
-	if (msg.includes('-> me')) {
-		console.log(msg);
-		var username = msg.slice(1, msg.indexOf('-> me') - 1);
-		if (msg.includes('clear')) {
-			client.channels.get('528307983856173062').send('The walls have been cleared by ' + username + ' via ingame message!');
+async function () {
+	await bot.on("message", function(message) {
+		var msg = message.toString();
+		if (msg.includes('-> me')) {
+			console.log(msg);
+			var username = msg.slice(1, msg.indexOf('-> me') - 1);
+			if (msg.includes('clear')) {
+				client.channels.get('528307983856173062').send('The walls have been cleared by ' + username + ' via ingame message!');
+			};
+		}
+		else {
+			client.channels.get('543650298410041344').send('``' + msg + '``');
 		};
-	}
-	else {
-		client.channels.get('543650298410041344').send('``' + msg + '``');
-	};
-});
+	});
+}
