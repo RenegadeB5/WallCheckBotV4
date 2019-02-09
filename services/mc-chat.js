31b5155c3eeae14951c70a08ed9ea93e83a9459a
RenegadeB5 <rocketmanB5@gmail.com>
2019-02-08 22:23:24 -0500

    Update mc-chat.js

--- services/mc-chat.js
+++ services/mc-chat.js
@@ -1,13 +1,20 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const mineflayer = require('mineflayer')
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
+if (process.argv.length < 4 || process.argv.length > 6) {
+	console.log('Usage : node echo.js <host> <port> [<name>] [<password>]')
+	process.exit(1)
+}
+
+const bot = mineflayer.createBot({
+
+	host: process.argv[2],
+	port: parseInt(process.argv[3]),
+	username: process.argv[4] ? process.argv[4] : 'echo',
+	password: process.argv[5],
+	verbose: true
 });
 
-io.on('connection', function(socket){
-	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
-	});
+bot.on('chat', (username, message) => {
+	if (username === bot.username) return
+	bot.chat(message)
 });
