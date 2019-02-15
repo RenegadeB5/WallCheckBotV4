5ebedb5e471eb5d7e13e79aa8b018d92b93db013
RenegadeB5 <rocketmanB5@gmail.com>
2019-02-15 15:47:06 -0500

    Update chat.js

--- services/chat.js
+++ services/chat.js
@@ -5,6 +5,7 @@ module.exports = {
 	type: "event",
 	on: {
 		message: async function (message) {
+			if (message.author.id === '373140966653034506') return;
 			if (message.channel.id !== '543650298410041344') return;
 			if (message.content.length >= 80) return;
 			if (message.content.includes('stamp')) {
