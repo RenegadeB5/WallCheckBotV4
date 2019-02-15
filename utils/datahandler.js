029f6d0d59d67471abb59992bc7383454ef5c123
RenegadeB5 <rocketmanB5@gmail.com>
2019-02-15 12:07:20 -0500

    Update datahandler.js

--- utils/datahandler.js
+++ utils/datahandler.js
@@ -57,9 +57,8 @@ class dataHandler {
 	async isRegistered(userid) {
 		let exiled = this.db.collection("factionPoints");
 		let member = await exiled.find({userid: userid}).toArray();
-		if (member[0] === undefined) return 'unregistered';
-		else if (member[0].ign === undefined) return 'noign';
-		else return 'registered';
+		if (member[0] !== undefined) {return 'registered';}
+		else {return 'unregistered';}
 	}
 		
 	async getLB(page) {
