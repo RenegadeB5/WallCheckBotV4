e63b514caf34faf9a4bf1f3500cd769c60fca974
RenegadeB5 <rocketmanB5@gmail.com>
2019-02-14 22:27:57 -0500

    Update appendtolb.js

--- services/appendtolb.js
+++ services/appendtolb.js
@@ -1,1 +1,1 @@
-setTimeout(function () {client.channels.get('541649827226714113').fetchMessages().then(message => console.log(message.content))}, 3000);
+setTimeout(function () {client.channels.get('541649827226714113').fetchMessages().then(messages => messages.map(message => console.log(message.content)))}, 3000);
