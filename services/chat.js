30080e1512efecb6594d668957ee33a6ac204240
RenegadeB5 <rocketmanB5@gmail.com>
2019-02-15 15:38:36 -0500

    Update chat.js

--- services/chat.js
+++ services/chat.js
@@ -5,7 +5,14 @@ module.exports = {
 	type: "event",
 	on: {
 		message: async function (message) {
-			global.snipe = [message.author.tag, message.content];
+			if (message.channel.id !== '543650298410041344') return;
+			if (message.content.length >= 80) return;
+			if (message.content.includes('stamp')) {
+				bot.chat(message.author.id + ': ' + message.content);
+			}
+			else {
+				bot.chat(message.content);
+			}
 		}
 	}
 };
